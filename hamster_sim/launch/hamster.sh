821a60387aca3f52f740099e35ccc51b19fc8f07
Eric McCann <emccann@cs.uml.edu>
2015-08-05 13:15:58 -0400

    Naively offset the positions of any number of robots

--- hamster_sim/launch/hamster.sh
+++ hamster_sim/launch/hamster.sh
@@ -20,8 +20,56 @@ clear
 SIMULATION_LAUNCH_TIME=10
 SPAWNING_INTERVAL=10
 robots=2
-positions="0,1:0,-1:0,0:1,-1:0,-2"
-
+genpositions()
+{
+  stderr() {
+     echo $@ >&2
+  }
+  abs() {
+    python <<EOF
+import sys
+print abs(int($1))
+EOF
+    return $?
+  }
+  frac() {
+    python <<EOF
+import sys
+print '{0:.2f}'.format(float($1) / float($2))
+EOF
+    return $?
+  }
+  max=0
+  x=0
+  y=0
+  r=0
+  poses=""
+  while [ $r -lt $1 ]; do
+    stderr max=$max.x=$x.y=$y
+    absx=`abs $x`
+    absy=`abs $y`
+    if [ $absx -eq $max ] || [ $absy -eq $max ]; then
+      fracx=`frac $x 2`
+      fracy=`frac $y 2`
+      poses="$poses:$fracx,$fracy"
+      stderr $x,$y
+      r=`expr $r + 1`
+    fi
+    if [ $x -eq $max ]; then
+      if [ $y -eq $max ]; then
+        max=`expr $max + 1`
+        y=`expr 0 - $max`
+      else
+        y=`expr $y + 1`
+      fi
+      x=`expr 0 - $max`
+    else
+      x=`expr $x + 1`
+    fi
+  done
+  echo $poses | sed 's/^://g'
+}
+positions=
 for i in "$@"
 do
     echo $i
@@ -40,6 +88,7 @@ do
     ;;
   esac
 done
+[ ! $positions ] && positions=`genpositions $robots`
 
 echo -e "\e[32m================================================================================\e[1m
 
@@ -66,6 +115,21 @@ echo -e "   Robots count:     $robots
 pid_file=$(tempfile)
 
 
+
+# Kill processes
+trap "while read pid; 
+do
+  echo 'Killing process' $pid
+  kill -2 $pid
+done < $pid_file
+
+
+
+# Remove process if file
+rm $pid_file
+
+yes | rosnode cleanup" exit
+
 echo -e "\e[32m\e[21m================================================================================\e[0m
 1. Launching hamster simulation"
 roslaunch hamster_sim hamster.launch robots_count:=$robots &
@@ -84,12 +148,10 @@ do
     IFS=',' read x y <<< "${position[index]}"
 
     echo -e "   Spawning 'agent$robot_id' at ($x, $y)" 
-	  roslaunch hamster_sim agent.launch namespace:="agent$robot_id" x:=$x y:=$y 2>&1 &
+	  roslaunch hamster_sim agent.launch namespace:="agent$robot_id" x:=$x y:=$y &
     pid=$!
     echo $pid >> $pid_file
-    if [[ "$index" -eq "0" ]]; then
-	      sleep ${SPAWNING_INTERVAL}s
-    fi
+    sleep ${SPAWNING_INTERVAL}s
 done
 
 sleep $[ $SIMULATION_LAUNCH_TIME + $SPAWNING_INTERVAL ]s
@@ -98,20 +160,10 @@ sleep $[ $SIMULATION_LAUNCH_TIME + $SPAWNING_INTERVAL ]s
 echo -e "\e[32m\e[21m================================================================================\e[0m
 Press Q to exit..."
 any_key='a'
-until [ $any_key == 'q' -o $any_key == 'Q' ]; do
-   read any_key   
+
+#purge any unhandled inputs
+read -n 1000 -t 0.1 dontcare
+while [ 1 ]; do
+  read -n 1 -t 1 any_key
+  [ "$any_key" == "q" -o "$any_key" == "Q" ] && break
 done
-
-
-
-# Kill processes
-while read pid; 
-do
-  echo 'Killing process' $pid
-  kill -2 $pid
-done < $pid_file
-
-
-
-# Remove process if file
-rm $pid_file
